8e966ef39e3b8cc91dd4beaac3406f9ca08fc64b
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-04-02 13:14:32 -0400

    feat: add logout button to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 import { Link, useOutletContext } from "react-router-dom"
 import Preview from "./Preview"
+import { useContext } from "react"
+import AuthContext from "../contexts/authContext"
 
 const Home = () => {
 
     const [data] = useOutletContext()
+
+    const { logOut } = useContext(AuthContext)
+
+    const handleLogOut = (e) => {
+        e.preventDefault()
+        logOut()
+    }
 
     return (
         <>
@@ -14,6 +23,7 @@ const Home = () => {
             })}
             <Link to='/login'><h3>Click here to log in</h3></Link>
             <Link to='/register'><h3>Click here to register</h3></Link>
+            <button onClick={handleLogOut}>Log out</button>
         </>
     )
 }
