f93b9f7231bb17f7624a069287f8be05734acdaa
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-04-03 21:59:30 -0400

    feat: add textarea for new Post comment if user

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import { Link, useParams, useOutletContext } from "react-router-dom"
 import Comment from "./Comment";
+import AuthContext from "../contexts/authContext";
 
 const Post = () => {
 
     const { postId } = useParams();
     const [ data ] = useOutletContext()
+    const { user } = useContext(AuthContext)
 
     const postData = data.find(post => 
             post.id === Number(postId)
@@ -16,6 +19,14 @@ const Post = () => {
             {postData.comments.map(comment => {
                 return <Comment key={comment.id} content={comment.content} timestamp={comment.postedAt} />
             })}
+            {user ? (
+                <div id="new-comment">
+                    <label htmlFor="content">Add a new comment below: </label>
+                    <textarea id="content"></textarea>
+                </div>
+            ) : (
+                    <div id="no-user"><b>Sign in</b> to leave a comment</div>
+            )}
             <Link to='/'><h2>Go back to home <i>here</i></h2></Link>
         </>
     )
