6c1196ba38d944e2d8f1ab54eefb7858568d0512
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-04-03 22:06:21 -0400

    refactor: remove unnecessary Auth component/route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import Root from './components/Root'
 import Home from './components/Home'
 import Post from './components/Post'
-import Auth from './components/Auth'
 import LogIn from './components/LogIn'
 import Register from './components/Register'
 import './App.css'
@@ -15,10 +14,8 @@ function App() {
         <Route path='/' element={<Root />}>
           <Route index element={<Home />} />
           <Route path='posts/:postId' element={<Post />}  />
-          <Route element={<Auth />}>
-            <Route path='/login' element={<LogIn />} />
-            <Route path='/register' element={<Register />} />
-          </Route>
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/register' element={<Register />} />
         </Route>
       </Routes>
     </>
