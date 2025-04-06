58085e257f5c31793fc8b0f13b01ada3364bb6ec
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-04-06 14:04:40 -0400

    feat: style Form component, remove Login/Register

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Root from './components/Root'
 import Home from './components/Home/Home'
 import Post from './components/Post'
-import LogIn from './components/LogIn'
-import Register from './components/Register'
+import Form from './components/Form/Form'
 import './App.css'
 
 function App() {
@@ -14,8 +13,8 @@ function App() {
         <Route path='/' element={<Root />}>
           <Route index element={<Home />} />
           <Route path='posts/:postId' element={<Post />}  />
-          <Route path='/login' element={<LogIn />} />
-          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Form key='login' type='login'/>} />
+          <Route path='/register' element={<Form key='register' type='register' />} />
         </Route>
       </Routes>
     </>
