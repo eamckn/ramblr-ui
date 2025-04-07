48246599cb16382b6e7da86f2202d5eb0eccdca2
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-04-07 14:49:58 -0400

    feat: align Home title and preview display

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
     const [ postsData ] = useOutletContext()
 
     return (
-        <>
-            <h1>Home</h1>
+        <div id={styles.home}>
+            <h1 className={styles.title}>Home</h1>
             <div id={styles.previewDisplay}>
                 {postsData.map(post => {
                     return <Preview key={post.id} id={post.id} title={post.title} content={post.content} timestamp={post.postedAt} commentCount={post.comments.length} />
                 })}
             </div>
-        </>
+        </div>
     )
 }
 
