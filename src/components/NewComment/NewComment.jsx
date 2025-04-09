df5b3961afb73c83faae293c95d9601fe0e03756
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-04-09 14:52:11 -0400

    feat: build NewComment component for Post

---
+++ src/components/NewComment/NewComment.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect, useContext } from 'react';
+import { useParams, useOutletContext } from 'react-router-dom';
+import AuthContext from '../../contexts/authContext';
+import styles from './NewComment.module.css'
+
+const NewComment = () => {
+
+    const [content, setContent] = useState('')
+    const [submitting, setSubmitting] = useState(false)
+    const [submitError, setSubmitError] = useState(null)
+
+    const { postId } = useParams()
+    const [ data, getData ] = useOutletContext()
+    const { user } = useContext(AuthContext)
+
+    const handleCommentInput = (e) => {
+        e.preventDefault()
+        setContent(e.target.value)
+    }
+
+    const handleCommentSubmit = async (e) => {
+        try {
+            e.preventDefault()
+            setSubmitting(true)
+            const response = await fetch(`http://localhost:8080/posts/${postId}/comments`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ content, userId: user.id })
+            })
+            if (!response.ok) {
+                setSubmitError(new Error('Server error'))
+            } else {
+                getData()
+            }
+        } catch (error) {
+            setSubmitError(error)
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
+    if (submitting) return <div className={styles.submitting}>Sending comment...</div>
+
+    return (
+        <>
+            <form onSubmit={handleCommentSubmit} className={styles.newComment}>
+                <textarea className={styles.commentInput} rows='5' onChange={handleCommentInput} value={content} placeholder='Add a new comment' />
+                <button className={styles.commentButton} type='submit'>Post</button>
+            </form>
+            {submitError && <div className={styles.submitError}>There was an error trying to submit your comment. Please try again.</div>}
+        </>
+    )
+    
+}
+
+export default NewComment
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link, useParams, useOutletContext } from "react-router-dom"
 import Comment from "../Comment/Comment";
+import NewComment from "../NewComment/NewComment";
 import AuthContext from "../../contexts/authContext";
 import styles from './Post.module.css'
 
@@ -25,12 +26,9 @@ const Post = () => {
                     return <Comment key={comment.id} content={comment.content} timestamp={comment.postedAt} user={comment.user} />
                 })}
                 {user ? (
-                    <div id="new-comment">
-                        <label htmlFor="content">Add a new comment below: </label>
-                        <textarea id="content"></textarea>
-                    </div>
+                    <NewComment />
                 ) : (
-                        <div id="no-user"><Link to='/login'><b>Sign in</b></Link> to leave a comment</div>
+                    <div className={styles.noUser}><Link to='/login'><b>Sign in</b></Link> to leave a comment</div>
                 )}
             </div>
         </div>
