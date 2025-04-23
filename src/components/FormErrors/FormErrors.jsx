d4cbba671780782414072af22a46142c57612980
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-04-23 17:33:22 -0400

    fix: rerun FormError animation on each failed try

--- src/components/FormErrors/FormErrors.jsx
+++ src/components/FormErrors/FormErrors.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react'
 import styles from './FormErrors.module.css'
 
 const FormError = ({ errors }) => {
 
+    const [timestamp, setTimestamp] = useState(Date.now())
+
+    /*
+    When new errors array is passed in (happens on every call),
+    force a state update that will be used as the key for the element.
+    This way, our animation will rerun on every continued failed
+    login or register attempt, letting our users know that their click
+    was processed, but there's still something wrong.
+    */
+    useEffect(() => {
+        setTimestamp(Date.now())
+    }, [ errors ])
+
     return (
 
-        <ul className={styles.formErrors}>
+        <ul className={styles.formErrors} key={timestamp}>
             {errors.map(error => {
                 return <li className={styles.error} key={error}>{error}</li>
             })}
